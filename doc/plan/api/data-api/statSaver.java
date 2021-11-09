0049d464dd73f8bdd2b6cfe9e57ac2b1f38d3a35
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-11-09 02:54:45 -0500

    added all api module files for data api and filled in sprint plan

---
+++ doc/plan/api/data-api/statSaver.java
@@ -0,0 +1,8 @@
+interface scoreSaver {
+
+  /**
+   * saves score data into json file
+   * @param scoreData, Collection keeping track of game data passed in from backend
+   */
+  public void saveScores(Collection scoreData)
+}

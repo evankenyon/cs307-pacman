f7b2af50d295ba73afc3a7261bd6a8c47e386e9e
Evan Kenyon <evan.kenyon@duke.edu>
2021-11-12 05:58:11 +0000

    Revert "Revert "Merge branch 'evankenyon' into 'master'""
    This reverts commit 5dcb8f2e6f6a602ae537241a4db12d6e7840d99d

---
+++ test/ooga/model/factories/ConsumableFactoryTest.java
@@ -0,0 +1,31 @@
+package ooga.model.factories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.InvocationTargetException;
+import ooga.model.agents.consumables.ghost;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConsumableFactoryTest {
+
+  private ConsumableFactory consumableFactory;
+
+  @BeforeEach
+  void setUp() {
+    consumableFactory = new ConsumableFactory();
+  }
+
+  @Test
+  void createConsumableWorking()
+      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    Assertions.assertTrue(consumableFactory.createConsumable("ghost") instanceof ghost);
+  }
+
+  @Test
+  void createConsumableBad() {
+    Assertions.assertThrows(ClassNotFoundException.class,
+        () -> consumableFactory.createConsumable("bad"));
+  }
+}

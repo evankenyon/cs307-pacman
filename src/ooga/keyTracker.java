495bbbba1107c4b16e1a85ae8ed7a5483876302a
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-11-15 16:00:03 -0500

    refactored keyTracker class to contain only a public method for processing the arrow key pressed by the user

--- src/ooga/keyTracker.java
+++ src/ooga/keyTracker.java
@@ -1,61 +1,35 @@
 package ooga;
 
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
-import java.util.List;
-import ooga.model.Agent;
-import ooga.model.Game;
-import ooga.model.GameBoard;
+
 
 public class keyTracker {
-  private int dx;
-  private int dy;
-  private GameBoard board; //how to actually get from instance of game ?
-
-  private void getPressedKey(KeyEvent event) {
-    if (event.getKeyCode() == KeyEvent.VK_LEFT) {
-      dx = -1;
-    }
-    else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
-      dx = 1;
-    }
-    else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-      dy = 1;
-    }
-    else if (event.getKeyCode() == KeyEvent.VK_UP) {
-     dy = -1;
-    }
-  }
-
-  //make sure no movement if hitting wall --> legal move
-
-  public void movePlayer(int currentX, int currentY) {
-    int spaceX = currentX + dx;
-    int spaceY = currentY + dy;
-    boolean isValid = validateMove(spaceX, spaceY);
-    if (isValid) {
-      //move player to [spaceX][spaceY]
-    }
-    else {
-      //send some indication to user that player cannot move here (maybe as simple as not moving)
-    }
-  }
 
   /**
-   * Checks whether player can be moved to new space
-   * @param spaceX
-   * @param spaceY
-   * @returns false if new space is a wall, true otherwise
+   * Communicates the key pressed by user to the model so that it can move the player avatar
+   * @param event is key pressed by player which should be one of the arrow keys
+   * @return String indicating which arrow key was pressed, or that the key was not an arrow key
    */
-  private boolean validateMove(int spaceX, int spaceY) {
-    List<List<Agent>> grid = board.getMyGrid();
-    Agent a = grid.get(spaceX).get(spaceY);
-    if (a.getAgentType().equals("wall")) {
-      return false;
+  public String getPressedKey(KeyEvent event) {
+    if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+      return "left";
+    }
+    else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
+      return "right";
+    }
+    else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+      return "down";
+    }
+    else if (event.getKeyCode() == KeyEvent.VK_UP) {
+     return "up";
     }
     else {
-      return true;
+      return "not-arrow";
     }
   }
+
+
+
+
 
 }

b0e3051d11a5e9bf9203caba8e40f7aed88d0d3c
Evan Kenyon <ek168@duke.edu>
2021-11-15 13:12:18 -0500

    Confirmed proper wall map creation, pursuant to issue #4

--- test/ooga/controller/IO/JsonParserTest.java
+++ test/ooga/controller/IO/JsonParserTest.java
@@ -2,7 +2,12 @@ package ooga.controller.IO;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Map;
+import ooga.model.util.AgentInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +19,38 @@ class JsonParserTest {
   @BeforeEach
   void setUp() {
     jsonParser = new JsonParser();
+  }
+
+  @Test
+  void properWallMapCreation() {
+    Map<String, List<AgentInfo>> expected  = new HashMap<>();
+
+    expected.put("Wall", new ArrayList<>());
+    for(int x = 0; x < 5; x++) {
+      expected.get("Wall").add(new AgentInfo(x, 0, 1));
+      expected.get("Wall").add(new AgentInfo(x, 2, 1));
+    }
+    expected.get("Wall").add(new AgentInfo(0, 1, 1));
+    expected.get("Wall").add(new AgentInfo(0, 4, 1));
+
+    expected.put("Pacman", new ArrayList<>());
+    expected.get("Pacman").add(new AgentInfo(1, 1, 1));
+
+    expected.put("Dot", new ArrayList<>());
+    expected.get("Dot").add(new AgentInfo(2, 1, 1));
+
+    expected.put("Pinky", new ArrayList<>());
+    expected.get("Pinky").add(new AgentInfo(3, 1, 1));
+
+    jsonParser.addWallMapConsumer(wallMap -> compareMaps(wallMap, expected));
+  }
+
+  private void compareMaps(Map<String, List<AgentInfo>> actual, Map<String, List<AgentInfo>> expected) {
+    for (String key : actual.keySet()) {
+      for (AgentInfo agentInfo : actual.get(key)) {
+        Assertions.assertTrue(expected.get(key).contains(agentInfo));
+      }
+    }
   }
 
   @Test

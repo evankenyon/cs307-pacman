3637e770d6aa8c3177f95e0541cb051a4e4bb719
Michelle Zhang <xmz@duke.edu>
2021-11-15 23:34:26 -0500

    testing wall and pellet

--- test/ooga/model/wallTest.java
+++ test/ooga/model/wallTest.java
@@ -1,8 +1,27 @@
 package ooga.model;
 
 import ooga.model.agents.wall;
+import ooga.model.util.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class wallTest {
-  private ooga.model.agents.wall wall;
 
+  private wall myWall;
+
+  @BeforeEach
+  void setUp() {
+    myWall = new wall(1, 2);
+  }
+
+  @Test
+  void stepTestNothing() {
+    Position myPosition = myWall.step();
+    int currentX = myPosition.getCoords()[0];
+    int currentY = myPosition.getCoords()[1];
+
+    Assertions.assertEquals(1, currentX);
+    Assertions.assertEquals(2, currentY);
+  }
 }

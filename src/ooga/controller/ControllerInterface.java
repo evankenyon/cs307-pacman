aadf8058847c70fec06f9c76c340c76a06de9537
Evan Kenyon <ek168@duke.edu>
2021-11-15 20:57:43 -0500

    Added dummy method for initial implementation of setting up initial states

--- src/ooga/controller/ControllerInterface.java
+++ src/ooga/controller/ControllerInterface.java
@@ -2,7 +2,10 @@ package ooga.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javafx.scene.input.KeyEvent;
+import ooga.model.util.Position;
 
 public interface ControllerInterface {
 

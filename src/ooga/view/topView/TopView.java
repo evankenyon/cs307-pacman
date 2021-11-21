fa52fcae9aae4a23421772474446163df4786bb5
Katherine Cottrell <kmc113@duke.edu>
2021-11-21 04:42:34 -0500

    Refactored topview

--- src/ooga/view/topView/TopView.java
+++ src/ooga/view/topView/TopView.java
@@ -1,20 +1,24 @@
 package ooga.view.topView;
 
-import java.util.function.Consumer;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import java.util.function.Consumer;
 import ooga.model.VanillaGame;
 
+
 public class TopView {
+    private String PAUSE_BUTTON = "https://theologygaming.com/wp-content/uploads/2014/08/Pause.png";
+    private String PLAY_BUTTON = "https://cdn-icons-png.flaticon.com/512/109/109197.png";
     private GridPane topGrid;
     private Label scoreDisplay;
     private VanillaGame myGame;
     private Consumer<Integer> scoreConsumer = i -> updateScoreDisplay(i);
+
 
     public TopView (VanillaGame game) {
         myGame = game;
@@ -22,19 +26,40 @@ public class TopView {
         initiateTopView();
     }
 
+
     private GridPane initiateTopView() {
+        topGrid = new GridPane();
+        topGrid.add(makeLoadSaveGP(), 1, 1);
+        topGrid.add(makePausePlayGP(), 1, 2);
+        topGrid.add(makeStatsGP(), 1,3);
+        return topGrid;
+    }
+
+    private GridPane makeLoadSaveGP() {
+        GridPane loadSaveGP = new GridPane();
         Button loadButton = makeButton("Load game", e -> loadGame());
         Button saveButton = makeButton("Save game", e -> saveGame());
-        Label lifeDisplay = updateLifeDisplay();
-        scoreDisplay = new Label("SCORE: ");
-
-        topGrid = new GridPane();
         topGrid.add(loadButton, 1, 1);
         topGrid.add(saveButton, 2, 1);
-        topGrid.add(lifeDisplay, 1,2);
-        topGrid.add(scoreDisplay, 1,3);
+        return loadSaveGP;
+    }
 
-        return topGrid;
+    private GridPane makePausePlayGP() {
+        GridPane pausePlayGP = new GridPane();
+        ImageView playButton = makeImgButton(PLAY_BUTTON, e -> playGame());
+        pausePlayGP.add(playButton, 1, 1);
+        ImageView pauseButton = makeImgButton(PAUSE_BUTTON, e -> pauseGame());
+        pausePlayGP.add(pauseButton, 2, 1);
+        return pausePlayGP;
+    }
+
+    private GridPane makeStatsGP() {
+        GridPane statsGP = new GridPane();
+        Label lifeDisplay = updateLifeDisplay();
+        scoreDisplay = new Label("SCORE: ");
+        statsGP.add(lifeDisplay, 1,1);
+        statsGP.add(scoreDisplay, 2,1);
+        return statsGP;
     }
 
     private void loadGame() {
@@ -42,6 +67,14 @@ public class TopView {
     }
 
     private void saveGame() {
+        // TODO: Implement
+    }
+
+    private void playGame() {
+        // TODO: Implement
+    }
+
+    private void pauseGame() {
         // TODO: Implement
     }
 
@@ -60,6 +93,15 @@ public class TopView {
         Button myButton = new Button(name);
         myButton.setOnAction(handler);
         return myButton;
+    }
+
+    private ImageView makeImgButton(String imgFile, EventHandler handler) {
+        Image myImage = new Image(imgFile);
+        ImageView imgButton = new ImageView(myImage);
+        imgButton.setFitWidth(50);
+        imgButton.setFitHeight(50);
+        imgButton.setOnMouseReleased(handler);
+        return imgButton;
     }
 
     public GridPane getTopViewGP() {

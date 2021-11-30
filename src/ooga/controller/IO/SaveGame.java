376799007b10417c256ecffb7a979bae3ccc2384
dfernandez2000 <85184614+dfernandez2000@users.noreply.github.com>
2021-11-30 17:45:42 -0500

    SaveGame with simplified input files

--- src/ooga/controller/IO/SaveGame.java
+++ src/ooga/controller/IO/SaveGame.java
@@ -13,13 +13,7 @@ public class SaveGame {
   private static StringBuilder path = new StringBuilder();
 
   //json values
-  private static String Title;
-  private static String Player;
-  private static String requiredPellets;
-  private static String optionalPellets;
-  private static String powerUps;
   private static String numberOfLives;
-  private static String opponentTypes;
   private static String difficultyLevel;
   private static String wallMap;
 
@@ -35,14 +29,12 @@ public class SaveGame {
     try {
       FileWriter fileToSave = new FileWriter(jsonFile);
       JSONObject configObject = new JSONObject();
-      Player = "Pac-Man"; //default setting until made dynamic
-      configObject.put("Player", Player);
-      requiredPellets = "Test-RequiredPellets"; //assume it will be pellet types found in map; dcurrently default
-      configObject.put("RequiredPellets", requiredPellets);
       numberOfLives = "Test-Number-Of-Lives";
       configObject.put("NumberOfLives", numberOfLives);
-      difficultyLevel = "Test-Wall-Map";
+      difficultyLevel = "Test-Difficulty-Level";
       configObject.put("WallMap", difficultyLevel);
+      wallMap ="Test-Wall-Map";
+      configObject.put("WallMap", wallMap);
       fileToSave.write(String.valueOf(configObject));
       fileToSave.close();
     } catch (IOException e) {

603499f17caabfb65a93089099fd5308aa2cb8ee
Evan Kenyon <ek168@duke.edu>
2021-12-05 14:41:25 -0500

    Updated all valid data files to no longer have extranoues keys

--- src/ooga/controller/IO/utils/JSONObjectParser.java
+++ src/ooga/controller/IO/utils/JSONObjectParser.java
@@ -15,6 +15,8 @@ import org.json.JSONObject;
 public class JSONObjectParser {
 
   public static JSONObject parseJSONObject(File file) throws IOException, JSONException {
+    //Borrowed method for turning file into String from
+    // https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
     return new JSONObject(new String(Files.readAllBytes(Path.of(file.getPath()))));
   }
 }

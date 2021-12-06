e4a6337b0266a16fd526144a821faadf3f997347
Evan Kenyon <ek168@duke.edu>
2021-12-06 18:56:01 -0500

    Finished rounding out JsonParser tests

--- test/ooga/controller/IO/JsonParserTest.java
+++ test/ooga/controller/IO/JsonParserTest.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 import ooga.controller.IO.utils.JSONObjectParser;
 import ooga.model.util.Position;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,29 +27,7 @@ class JsonParserTest {
 
   @Test
   void properWallMapCreation() throws IOException {
-    Map<String, List<Position>> expected = new HashMap<>();
-
-    expected.put("Wall", new ArrayList<>());
-    for (int x = 0; x < 5; x++) {
-      expected.get("Wall").add(new Position(x, 0));
-      expected.get("Wall").add(new Position(x, 2));
-    }
-    expected.get("Wall").add(new Position(0, 1));
-    expected.get("Wall").add(new Position(4, 1));
-
-    expected.put("Pacman", new ArrayList<>());
-    expected.get("Pacman").add(new Position(1, 1));
-
-    expected.put("Dot", new ArrayList<>());
-    expected.get("Dot").add(new Position(2, 1));
-
-    expected.put("Pinky", new ArrayList<>());
-    expected.get("Pinky").add(new Position(3, 1));
-
-    jsonParser.addVanillaGameDataConsumer(
-        vanillaGameDataInterface -> compareWallMaps(vanillaGameDataInterface.wallMap(), expected));
-
-    jsonParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/basicWallMap.json")));
+    Map<String, List<Position>> expected = properWallMapCreationSetup();
   }
 
   private void compareWallMaps(Map<String, List<Position>> actual,
@@ -74,7 +54,8 @@ class JsonParserTest {
     String expected = "Pacman";
     jsonParser.addVanillaGameDataConsumer(
         vanillaGameDataInterface -> comparePlayers(vanillaGameDataInterface.player(), expected));
-    jsonParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/basicWallMap.json")));
+    jsonParser.parseJSON(
+        JSONObjectParser.parseJSONObject(new File("data/tests/basicWallMap.json")));
   }
 
   private void comparePlayers(String actual, String expected) {
@@ -89,7 +70,8 @@ class JsonParserTest {
     jsonParser.addVanillaGameDataConsumer(
         vanillaGameDataInterface -> comparePelletMaps(vanillaGameDataInterface.pelletInfo(),
             expected));
-    jsonParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/basicWallMap.json")));
+    jsonParser.parseJSON(
+        JSONObjectParser.parseJSONObject(new File("data/tests/basicWallMap.json")));
   }
 
   private void comparePelletMaps(Map<String, Boolean> actual, Map<String, Boolean> expected) {
@@ -116,7 +98,8 @@ class JsonParserTest {
       jsonParser.parseJSON(
           JSONObjectParser.parseJSONObject(new File("data/tests/extraKeys.json")));
     } catch (Exception e) {
-      Assertions.assertEquals(String.format("Unexpected key %s was found in file", "BadKey"), e.getMessage());
+      Assertions.assertEquals(String.format("Unexpected key %s was found in file", "BadKey"),
+          e.getMessage());
     }
     Assertions.assertThrows(InputMismatchException.class, () -> jsonParser.parseJSON(
         JSONObjectParser.parseJSONObject(new File("data/tests/extraKeys.json"))));
@@ -126,7 +109,8 @@ class JsonParserTest {
   @Test
   void uploadBadFile() {
     Assertions.assertThrows(IOException.class,
-        () -> jsonParser.parseJSON(JSONObjectParser.parseJSONObject(new File("./doc/plan/data/nonexistent.json"))));
+        () -> jsonParser.parseJSON(
+            JSONObjectParser.parseJSONObject(new File("./doc/plan/data/nonexistent.json"))));
   }
 
   @Test
@@ -147,7 +131,8 @@ class JsonParserTest {
       jsonParser.parseJSON(
           JSONObjectParser.parseJSONObject(new File("data/tests/duplicateGhosts.json")));
     } catch (Exception e) {
-      Assertions.assertEquals("Game cannot have more than one of each type of ghost", e.getMessage());
+      Assertions.assertEquals("Game cannot have more than one of each type of ghost",
+          e.getMessage());
     }
     Assertions.assertThrows(InputMismatchException.class, () -> jsonParser.parseJSON(
         JSONObjectParser.parseJSONObject(new File("data/tests/duplicateGhosts.json"))));
@@ -159,7 +144,9 @@ class JsonParserTest {
       jsonParser.parseJSON(
           JSONObjectParser.parseJSONObject(new File("data/tests/missingRequiredPellet.json")));
     } catch (Exception e) {
-      Assertions.assertEquals(String.format("Game does not contain any of the %s required pellets", "Dot"), e.getMessage());
+      Assertions.assertEquals(
+          String.format("Game does not contain any of the %s required pellets", "Dot"),
+          e.getMessage());
     }
     Assertions.assertThrows(InputMismatchException.class, () -> jsonParser.parseJSON(
         JSONObjectParser.parseJSONObject(new File("data/tests/missingRequiredPellet.json"))));
@@ -203,5 +190,54 @@ class JsonParserTest {
     Assertions.assertEquals(expectedCols, jsonParser.getCols());
   }
 
-  //TODO: add tests for reset and for JSONException
+  @Test
+  void badJsonThrowsJSONException() {
+    Assertions.assertThrows(JSONException.class, () -> jsonParser.parseJSON(new JSONObject("bad")));
+  }
+
+  @Test
+  void properReset() throws IOException {
+    Map<String, List<Position>> expected = properWallMapCreationSetup();
+
+    expected.clear();
+    expected.put("Wall", new ArrayList<>());
+    expected.get("Wall").add(new Position(0, 0));
+
+    expected.put("Pacman", new ArrayList<>());
+    expected.get("Pacman").add(new Position(1, 0));
+
+    expected.put("Dot", new ArrayList<>());
+    expected.get("Dot").add(new Position(2, 0));
+
+    jsonParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/simple.json")));
+  }
+
+  private Map<String, List<Position>> properWallMapCreationSetup() throws IOException {
+    Map<String, List<Position>> expected = new HashMap<>();
+
+    expected.put("Wall", new ArrayList<>());
+    for (int x = 0; x < 5; x++) {
+      expected.get("Wall").add(new Position(x, 0));
+      expected.get("Wall").add(new Position(x, 2));
+    }
+    expected.get("Wall").add(new Position(0, 1));
+    expected.get("Wall").add(new Position(4, 1));
+
+    expected.put("Pacman", new ArrayList<>());
+    expected.get("Pacman").add(new Position(1, 1));
+
+    expected.put("Dot", new ArrayList<>());
+    expected.get("Dot").add(new Position(2, 1));
+
+    expected.put("Pinky", new ArrayList<>());
+    expected.get("Pinky").add(new Position(3, 1));
+
+    jsonParser.addVanillaGameDataConsumer(
+        vanillaGameDataInterface -> compareWallMaps(vanillaGameDataInterface.wallMap(), expected));
+
+    jsonParser.parseJSON(
+        JSONObjectParser.parseJSONObject(new File("data/tests/basicWallMap.json")));
+    return expected;
+  }
+
 }

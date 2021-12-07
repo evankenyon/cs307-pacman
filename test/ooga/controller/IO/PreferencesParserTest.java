072cc31d0608ab04bf4d1729c2b8876b9f68fb34
Evan Kenyon <ek168@duke.edu>
2021-12-06 20:23:32 -0500

    Replaced deprecated method in PreferencesParser tests

--- test/ooga/controller/IO/PreferencesParserTest.java
+++ test/ooga/controller/IO/PreferencesParserTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.InputMismatchException;
 import java.util.List;
+import ooga.controller.IO.utils.JSONObjectParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,24 +22,24 @@ class PreferencesParserTest {
 
   @Test
   void invalidKey() {
-    Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.uploadFile(new File("data/tests/preferences/invalidKey.json")));
+    Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidKey.json"))));
   }
 
   @Test
   void invalidImagePath() {
-    Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.uploadFile(new File("data/tests/preferences/invalidImagePath.json")));
+    Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidImagePath.json"))));
   }
 
   @Test
   void invalidColor() {
-    Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.uploadFile(new File("data/tests/preferences/invalidColor.json")));
+    Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidColor.json"))));
   }
 
   @Test
   void getImagePathsPacman()
-      throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+      throws IOException, NoSuchMethodException, IllegalAccessException {
     String expected = "images/fruit.png";
-    preferencesParser.uploadFile(new File("data/tests/preferences/pacmanImagePath.json"));
+    preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/pacmanImagePath.json")));
     Assertions.assertEquals(expected, preferencesParser.getImagePaths().get("Pacman"));
   }
 
@@ -46,7 +47,7 @@ class PreferencesParserTest {
   void getColorDot()
       throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
     List<Double> expected = List.of(new Double[]{0.0, 0.5, 0.25});
-    preferencesParser.uploadFile(new File("data/tests/preferences/dotColor.json"));
+    preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/dotColor.json")));
     for (int index = 0; index < expected.size(); index++) {
       Assertions.assertEquals(expected.get(index), preferencesParser.getColors().get("Dot").get(index));
     }
@@ -55,17 +56,17 @@ class PreferencesParserTest {
 
   @Test
   void getStyleLight()
-      throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+      throws IOException, NoSuchMethodException, IllegalAccessException {
     String expected = "Light";
-    preferencesParser.uploadFile(new File("data/tests/preferences/lightStyle.json"));
+    preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/lightStyle.json")));
     Assertions.assertEquals(expected, preferencesParser.getStyle());
   }
 
   @Test
   void getStartingConfig()
-      throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+      throws IOException, NoSuchMethodException, IllegalAccessException {
     String expected = "test_implementation.json";
-    preferencesParser.uploadFile(new File("data/tests/preferences/startingConfig.json"));
+    preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/startingConfig.json")));
     Assertions.assertEquals(expected, preferencesParser.getStartingConfig().getName());
   }
 }

f35431bb966947546250a43a5c02fae51c7c84c4
Evan Kenyon <ek168@duke.edu>
2021-12-06 21:38:24 -0500

    Added assertions for exception messages to PreferencesParser tests

--- test/ooga/controller/IO/PreferencesParserTest.java
+++ test/ooga/controller/IO/PreferencesParserTest.java
@@ -1,10 +1,7 @@
 package ooga.controller.IO;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.InputMismatchException;
 import java.util.List;
 import ooga.controller.IO.utils.JSONObjectParser;
@@ -22,30 +19,45 @@ class PreferencesParserTest {
 
   @Test
   void invalidKey() {
+    try {
+      preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidKey.json")));
+    } catch (InputMismatchException | IOException | NoSuchMethodException e) {
+      Assertions.assertEquals("The uploaded file does not have enough keys", e.getMessage());
+    }
     Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidKey.json"))));
   }
 
   @Test
   void invalidImagePath() {
+    try {
+      preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidImagePath.json")));
+    } catch (InputMismatchException | IOException | NoSuchMethodException e) {
+      Assertions.assertEquals(String.format("Invalid image path %s was passed in for key %s", "bad", "Pacman"), e.getMessage());
+    }
     Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidImagePath.json"))));
   }
 
   @Test
   void invalidColor() {
+    try {
+      preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidColor.json")));
+    } catch (InputMismatchException | IOException | NoSuchMethodException e) {
+      Assertions.assertEquals(String.format("Invalid rgb value of %s", "2.0"), e.getMessage());
+    }
     Assertions.assertThrows(InputMismatchException.class, () -> preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/invalidColor.json"))));
   }
 
   @Test
   void getImagePathsPacman()
-      throws IOException, NoSuchMethodException, IllegalAccessException {
-    String expected = "images/fruit.png";
+      throws IOException, NoSuchMethodException {
+    String expected = "data/images/fruit.png";
     preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/pacmanImagePath.json")));
     Assertions.assertEquals(expected, preferencesParser.getImagePaths().get("Pacman"));
   }
 
   @Test
   void getColorDot()
-      throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+      throws IOException, NoSuchMethodException {
     List<Double> expected = List.of(new Double[]{0.0, 0.5, 0.25});
     preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/dotColor.json")));
     for (int index = 0; index < expected.size(); index++) {
@@ -56,7 +68,7 @@ class PreferencesParserTest {
 
   @Test
   void getStyleLight()
-      throws IOException, NoSuchMethodException, IllegalAccessException {
+      throws IOException, NoSuchMethodException {
     String expected = "Light";
     preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/lightStyle.json")));
     Assertions.assertEquals(expected, preferencesParser.getStyle());
@@ -64,7 +76,7 @@ class PreferencesParserTest {
 
   @Test
   void getStartingConfig()
-      throws IOException, NoSuchMethodException, IllegalAccessException {
+      throws IOException, NoSuchMethodException {
     String expected = "test_implementation.json";
     preferencesParser.parseJSON(JSONObjectParser.parseJSONObject(new File("data/tests/preferences/startingConfig.json")));
     Assertions.assertEquals(expected, preferencesParser.getStartingConfig().getName());

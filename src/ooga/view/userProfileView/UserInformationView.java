9f6faf7acd709e95ec8fd1de076c8fc13a7d7142
Dane Erickson <dte12@duke.edu>
2021-12-06 23:44:37 -0500

    made user profile view tests

--- src/ooga/view/userProfileView/UserInformationView.java
+++ src/ooga/view/userProfileView/UserInformationView.java
@@ -1,14 +1,10 @@
 package ooga.view.userProfileView;
 
-import static ooga.view.center.agents.MovableView.IMAGE_PATH;
 import static ooga.view.startupView.GameStartupPanel.EXAMPLES_PATH;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.List;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -18,8 +14,6 @@ import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -108,6 +102,7 @@ public class UserInformationView {
 
   private void addTextInfo(GridPane root, String key, String value, int columnIndex, int rowIndex) {
     Text textInfo = new Text(String.format("%s: %s", key, value));
+    textInfo.setId(key);
     root.add(textInfo, columnIndex, rowIndex);
   }
 
